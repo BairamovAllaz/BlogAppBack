7dd4e3471eca3ff41494aff186816881d1c7cb79
BairamovAllaz <socialbairamovi2004@gmail.com>
2025-05-13 21:40:52 +0400

    add preview

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,19 +1,38 @@
 const pool = require("../db");
+const crypto = require("crypto");
 
-
-const CreatePost = async (req, res) => {
-  const { email, post_txt, create_time, update_time,status,like_count } = req.body;
-  try {
-    await pool.query(
-      "INSERT INTO posts (email, post_txt,create_time,update_time,status, like_count) VALUES (?, ?, ?, ?, ?, ?)",
-      [email, post_txt, create_time, update_time, status, like_count]
-    );
-    res.status(201).json("Post created succsesfly");
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong while creating post" });
-  }
+const generatePreviewToken = () => {
+  return crypto.randomBytes(16).toString("hex");
 };
 
+const CreatePost = async (req, res) => {
+  const { email, post_txt, create_time, update_time, status, like_count } =
+    req.body;
+  try {
+    const [data] = await pool.query(
+      "INSERT INTO posts (email, post_txt,create_time,update_time,status, like_count, preview_token) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      [
+        email,
+        post_txt,
+        create_time,
+        update_time,
+        status,
+        like_count,
+        generatePreviewToken(),
+      ]
+    );
 
+     const insertedId = data.insertId;
+     const [insertedData] = await pool.query(
+       "SELECT * FROM posts WHERE id = ?",
+       [insertedId]
+     );
+    res.status(201).json({ insertedData });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong while creating post" });
+  }
+};
 
 module.exports = { CreatePost };
